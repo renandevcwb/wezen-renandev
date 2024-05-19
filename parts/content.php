fe51fd5179f94d78d1ae1453d7fbe8e5914d49d7
renandevcwb <renandevcwb@gmail.com>
2024-05-19 19:12:15 -0300

    Tinha ficado estranho

--- parts/content.php
+++ parts/content.php
@@ -13,14 +13,12 @@
     <section class="row posts-section">
         <?php if( is_single() ):?>
         <!-- Início dos Dados do Autor -->
-        <div class="row py-3">
-            <div class="card card-author border-0 d-flex">
-                <div class="col">
-                <?php echo get_avatar( get_the_author_meta( 'user_email' ), 62, 'robohash', 'Avatar de Autor', array( 'class' => 'img-author rounded-circle' ) );?>
-                </div>
-                <div class="col">
-                <span class="card-title author-title">Publicado por <?php echo get_the_author();?></span>
-                <span class="card-text author-description"><?php echo esc_html( the_author_meta( 'description' ) );?></span>
+        <div class="row py-3 d-flex py-3">
+            <div class="card card-author border-0">
+                <?php echo get_avatar( get_the_author_meta( 'user_email' ), 62, 'robohash', 'Avatar de Autor', array( 'class' => 'card-img rounded-circle' ) );?>
+                <div class="card-body">
+                    <h6 class="card-title author-title">Publicado por <?php echo get_the_author();?></h6>
+                    <p class="card-text author-description"><?php echo esc_html( the_author_meta( 'description' ) );?></p>
                 </div>
             </div>
         </div>
