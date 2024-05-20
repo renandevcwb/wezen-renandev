01e42a8cb170b6a3bc76f831e23f28c4385547b7
renandevcwb <renandevcwb@gmail.com>
2024-05-20 14:26:58 -0300

    asasass

--- components/forms/form-search.php
+++ components/forms/form-search.php
@@ -11,7 +11,7 @@
 <form  class="form-group d-flex wezen-search" action="<?php echo esc_url( home_url( '/' ) );?>" method="get" role="search">
     <input class="form-control wezen-control rounded-0" type="search" placeholder="O que você está procurando?" autocomplete="on" name="s"/>
     <button type="submit" class="btn btn-search rounded-0">
-        <i class="fa-solid fa-magnifying-glass"></i> 
+        Pesquisar
     </button>
 </form>
 <!-- Fim do Formulário de Busca -->
