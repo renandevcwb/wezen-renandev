14708824c632ed943e8e3ff0cb18f5e40308e8a2
renandevcwb <renandevcwb@gmail.com>
2024-05-20 14:30:27 -0300

    gagagag

--- components/forms/form-search.php
+++ components/forms/form-search.php
@@ -8,7 +8,7 @@
  * 
  */?>
 <!-- Início do Formulário de Busca -->
-<form  class="form-group d-flex wezen-search" action="<?php echo esc_url( home_url( '/' ) );?>" method="get" role="search">
+<form  class="form-group d-flex wezen-search w-100" action="<?php echo esc_url( home_url( '/' ) );?>" method="get" role="search">
     <input class="form-control wezen-control rounded-0" type="search" placeholder="O que você está procurando?" autocomplete="on" name="s"/>
     <button type="submit" class="btn btn-search rounded-0">
         Pesquisar
